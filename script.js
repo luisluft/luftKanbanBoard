64e97870538fdab6a1af7493c890924c17417527
luisluft <luberluft@gmail.com>
2021-08-31 07:25:04 -0400

    add local storage feature

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Multiple elements
 const addButtons = document.querySelectorAll(".add-button:not(.solid)");
 const saveItemButtons = document.querySelectorAll(".solid");
 const addItemContainers = document.querySelectorAll(".add-container");
 const addItems = document.querySelectorAll(".add-item");
-// Item Lists
 const itemLists = document.querySelectorAll(".drag-item-list");
+
+// Single elements
 const backlogList = document.getElementById("backlog-list");
 const progressList = document.getElementById("progress-list");
 const completeList = document.getElementById("complete-list");
@@ -16,6 +18,7 @@ let backlogListArray = [];
 let progressListArray = [];
 let completeListArray = [];
 let onHoldListArray = [];
+let listArrays = [];
 
 // Drag Functionality
 
@@ -34,12 +37,17 @@ function getSavedColumns() {
   }
 }
 
+getSavedColumns();
+updateSavedColumns();
+
 // Set localStorage Arrays
 function updateSavedColumns() {
-  localStorage.setItem("backlogItems", JSON.stringify(backlogListArray));
-  localStorage.setItem("progressItems", JSON.stringify(progressListArray));
-  localStorage.setItem("completeItems", JSON.stringify(completeListArray));
-  localStorage.setItem("onHoldItems", JSON.stringify(onHoldListArray));
+  listArrays = [backlogListArray, progressListArray, completeListArray, onHoldListArray];
+  const arrayNames = ["backlog", "progress", "complete", "onHold"];
+
+  arrayNames.forEach((arrayName, index) => {
+    localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]));
+  });
 }
 
 // Create DOM Elements for each list item
